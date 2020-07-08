8acf3a5113863f690f2a71b203c4d0eee8cf661c
Timo Klok <mail@timoklok.nl>
2020-07-08 20:54:29 +0200

    added basic starwars-api graphql calls

---
+++ src/Api.js
@@ -0,0 +1,107 @@
+import axios from "axios";
+
+const API_URL = 'https://swapi.graph.cool/';
+
+/**
+ * @description Function that retrieves characters that matches all three params provided
+ * @param {*} episodeId
+ * @param {*} homeworld
+ * @param {*} species
+ * @returns Promise
+ */
+export const getCharacters = async function (episodeId, homeworld, species) {
+
+	const query = `query findCharacters ($homeworld: String!, $species: String!, $episodeId: Int!) {
+		allPersons(
+			orderBy: name_ASC,
+			filter: {
+				homeworld: { name: $homeworld },
+				species_some: { name: $species },
+				films_some: { episodeId: $episodeId  }
+			}) {
+				id
+				name
+				homeworld {
+					name
+				}
+				species {
+					name
+				}
+			}
+		}`;
+
+	const variables = {
+		episodeId: episodeId,
+		homeworld: homeworld,
+		species: species
+	};
+
+	return runQuery(query, variables);
+
+}
+
+/**
+ * @description Function that retrieves all Star Wars Films
+ * @returns Promise
+ */
+export const getFilms = async function () {
+	const query =
+		`query allFilms {
+		  allFilms{
+	    	episodeId
+		    title
+		  }
+		}`
+	return runQuery(query);
+
+}
+
+/**
+ * @description Function that retrieves all Star Wars Species
+ * @returns Promise
+ */
+export const getSpecies = async function () {
+	const query =
+		`query allSpecies {
+		  allSpecies{
+	    	name
+		    id
+		  }
+		}`;
+
+	return runQuery(query);
+}
+
+/**
+ * @description Function that retrieves all Star Wars Worlds
+ * @returns Promise
+ */
+export const getHomeworlds = () => {
+	const query = `query allPlanets {
+		  allPlanets{
+	    	name
+		    id
+		  }
+		}`;
+	return runQuery(query);
+}
+
+
+/**
+ * @description Calls the Star Wars GraphQL
+ * @param {object} query 
+ * @param {object} variables
+ * @returns Promise
+ */
+const runQuery = async (query, variables = {}) => {
+
+	return await axios.post(API_URL, {
+		query: query,
+		variables: variables
+	}, {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+
+};
--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getFilms } from './Api';
 
 function App() {
+
+  getFilms().then((data) => { console.log(data) });
+
   return (
     <div className="App">
       <header className="App-header">
