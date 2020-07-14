e36aeb332c25423aba3158c882014bb6986d1afd
Timo Klok <mail@timoklok.nl>
2020-07-14 20:11:48 +0200

    remove dropdown when item is selected, replace with 'clear' button

--- src/components/OptionSelect.js
+++ src/components/OptionSelect.js
@@ -2,6 +2,7 @@ import React , { useContext, useEffect, useState } from "react";
 import SearchContext from '../context/SearchContext';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
 
 /**
  * @description Functional component that renders select element and updates global context when value changes
@@ -12,16 +13,28 @@ const OptionSelect = ({ list, listName }) => {
 	// @Todo: this is too specific  
 	if (listName === 'episodeId') {
 		list = list.map((item) => {
-			return { ...item, 'id': item.episodeId, 'name': item.title }
+			return { ...item, 'id': parseInt(item.episodeId), 'name': item.title }
 		});
 	}
 	
 	const [searchContextItems, setSearchContextItems] = useContext(SearchContext);
 	const [options, setOptions] = useState(list);
+	const [currentSelection, setCurrentSlection] = useState("");
 
+	const clearSelection = () => {
+		setCurrentSlection(undefined);
+		const searchParameters = { ...searchContextItems['searchParameters'], [listName]: undefined };
+		setSearchContextItems({ ...searchContextItems, searchParameters: searchParameters });
+	};
+
+	// set searchparameters in context object on selecting a option
 	const handleChange = (value) => {
-		console.log(value);
 		if (!value) value = undefined;
+		
+		// without truthy check we have to parse the episodeId to Int
+		const currentSelectionObject = list.filter((item) =>  (item.id == value));
+		setCurrentSlection(currentSelectionObject[0]['name'] );
+
 		const searchParameters = { ...searchContextItems['searchParameters'], [listName]: (listName === 'episodeId') ? parseInt(value) : value };
 		setSearchContextItems({ ...searchContextItems, searchParameters: searchParameters});
 	}
@@ -66,16 +79,40 @@ const OptionSelect = ({ list, listName }) => {
 		updateOptions(currentResult);
 	}, [searchContextItems.searchResult]);
 
-	return (<DropdownButton id="dropdown-basic-button" title={"Select"} onSelect={handleChange}>
-		{/* only show empy selection there's a selection */}
-		<Dropdown.Item eventKey=''>----</Dropdown.Item>
-		{options.map((item) => {
-			if ( !item.hidden ) {
-				return <Dropdown.Item key={item.id} eventKey={item.id}>{item.name}</Dropdown.Item>;
-			}
-			return undefined;
-		})}
-	</DropdownButton> );
+	const renderDropdown = () => {
+		if (currentSelection) {
+			return false;
+		}
+		return (
+			<DropdownButton id="dropdown-basic-button" title={(currentSelection) ? currentSelection : 'select'} onSelect={handleChange}>
+				{options.map((item) => {
+					if (!item.hidden) {
+						return <Dropdown.Item key={item.id} eventKey={item.id}>{item.name}</Dropdown.Item>;
+					}
+					return undefined;
+				})}
+			</DropdownButton> 
+		)
+	}
+
+	const renderSelection = () => {
+		if ( ! currentSelection  ) {
+			return false;
+		}
+		return (
+			<p>{currentSelection}
+				<Button onClick={clearSelection} variant="primary">clear</Button>
+			</p>
+		);
+	}
+
+	return (
+		<div>		
+			{renderSelection()}
+			{renderDropdown()}
+			
+		</div>
+	);
 
 }
 
